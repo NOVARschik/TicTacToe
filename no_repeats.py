74ea6e676de3d3586d69759d0b9d8d3f8797196c
NOVARschik <74792649+NOVARschik@users.noreply.github.com>
2022-02-08 20:06:19 +0300

    No_repeats

--- no_repeats.py
+++ no_repeats.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 data = pd.read_csv('TTT_dataset.csv')
 data = data.drop_duplicates()
-data.to_csv('TTT_dataset.csv')
+data.to_csv('TTT_dataset.csv', index=False)
 
 print('TTT_dataset.csv has no repeats')
